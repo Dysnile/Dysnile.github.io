57786d3a3e528b47b5e464bbe96a87b0266619ee
Dysnile <1039906249@qq.com>
2018-08-02 16:31:09 +0800

    Site updated: 2018-08-02 16:31:06

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: '云烟成雨',
+        artist: '房东的猫',
+        url: 'http://www.ytmp3.cn/down/48895.mp3',
+        cover: 'http://jijiantuchuang123.oss-cn-shanghai.aliyuncs.com/18-8-2/58394974.jpg',
+      }
+    ]
+});
+
