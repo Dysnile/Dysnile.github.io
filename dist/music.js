201363e134c4b7f21fb253d084b5b17dc9582ea6
Dysnile <1039906249@qq.com>
2018-08-19 12:28:59 +0800

    Site updated: 2018-08-19 12:28:56

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: '云烟成雨',
         artist: '房东的猫',
-        url: '  http://music.163.com/song/media/outer/url?id=513360721.mp3',
+        url: 'https://filemusic.oss-cn-shanghai.aliyuncs.com/%E4%BA%91%E7%83%9F%E6%88%90%E9%9B%A8%2B%E6%88%BF%E4%B8%9C%E7%9A%84%E7%8C%AB.mp3',
         cover: 'http://jijiantuchuang123.oss-cn-shanghai.aliyuncs.com/18-8-19/99353195.jpg',
       }
     ]
