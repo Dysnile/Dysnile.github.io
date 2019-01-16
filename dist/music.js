70b79795984781f81f367951bb53491979ae12e6
Dysnile <1039906249@qq.com>
2019-01-16 18:01:10 +0800

    Site updated: 2019-01-16 18:01:09

--- dist/music.js
+++ dist/music.js
@@ -8,13 +8,13 @@ const ap = new APlayer({
         artist: '房东的猫',
         url: 'https://filemusic.oss-cn-shanghai.aliyuncs.com/%E4%BA%91%E7%83%9F%E6%88%90%E9%9B%A8%2B%E6%88%BF%E4%B8%9C%E7%9A%84%E7%8C%AB.mp3',
         cover: 'http://jijiantuchuang123.oss-cn-shanghai.aliyuncs.com/18-8-19/99353195.jpg',
-      }
+      },
       {
         name: 'Alone',
         artist: 'Halsey',
         url: 'http://filemusic.oss-cn-shanghai.aliyuncs.com/Alone%2B-%2BHalsey%252CBig%2BSean%252CStefflon%2BDon.mp3',
         cover: 'http://filemusic.oss-cn-shanghai.aliyuncs.com/QQ%E6%88%AA%E5%9B%BE20190116174946.png',
-      }
+      },
       {
         name: 'Bad_at_Love',
         artist: 'Halsey',
